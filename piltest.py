18802dfeebdb1a0743d1ff304fb5f6c48449bcd6
Josh <jpetrack@hmc.edu>
2016-05-04 15:19:51 -0700

    Removing some unnecessary files

--- piltest.py
+++
@@ -1,5 +0,0 @@
-from PIL import Image, ImageDraw
-im = Image.new("RGB", (512, 512), "white")
-draw = ImageDraw.Draw(im)
-draw.ellipse(((100, 100), (200, 200)), fill=128)
-im.save("HELLOWORLDTEST", "PNG")
